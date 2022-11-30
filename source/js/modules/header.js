0f3f19fdddcb60843467e4db6100b2cba2d9cb96
Anna <lutomskaya@gmail.com>
2022-11-30 19:43:07 +0300

    Сделала шапку, главный экран, преимущества, каталог, бронирование

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -1,17 +1,17 @@
 function clickOnToggle() {
-  const mainHeader = document.querySelector('.main-header__nav');
+  const mainHeader = document.querySelector('.main-header');
 
   const mainToggle = document.querySelector('[data-button]');
 
-  mainHeader.classList.remove('main-header__nav--nojs');
+  mainHeader.classList.remove('nojs');
 
   mainToggle.addEventListener('click', function () {
-    if (mainHeader.classList.contains('main-header__nav--opened')) {
-      mainHeader.classList.remove('main-header__nav--opened');
-      mainHeader.classList.add('main-header__nav--closed');
+    if (mainHeader.classList.contains('main-header--opened')) {
+      mainHeader.classList.remove('main-header--opened');
+      mainHeader.classList.add('main-header--closed');
     } else {
-      mainHeader.classList.remove('main-header__nav--closed');
-      mainHeader.classList.add('main-header__nav--opened');
+      mainHeader.classList.remove('main-header--closed');
+      mainHeader.classList.add('main-header--opened');
     }
   });
 }

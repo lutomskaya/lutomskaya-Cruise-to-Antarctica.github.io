7f3ceadbd498604ba8bc12fa4c91219411f0c870
Anna <lutomskaya@gmail.com>
2022-12-11 11:19:06 +0300

    Правки

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -1,7 +1,11 @@
+import {ScrollLock} from '../utils/scroll-lock';
+
 function clickOnToggle() {
   const mainHeader = document.querySelector('.main-header');
 
   const mainToggle = document.querySelector('[data-button]');
+
+  const scrollLock = new ScrollLock();
 
   mainHeader.classList.remove('nojs');
 
@@ -9,9 +13,11 @@ function clickOnToggle() {
     if (mainHeader.classList.contains('main-header--opened')) {
       mainHeader.classList.remove('main-header--opened');
       mainHeader.classList.add('main-header--closed');
+      scrollLock.enableScrolling();
     } else {
       mainHeader.classList.remove('main-header--closed');
       mainHeader.classList.add('main-header--opened');
+      scrollLock.disableScrolling();
     }
   });
 }
